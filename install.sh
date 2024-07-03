ff28516372f2748376d06cb2a6fc5ce42690198c
Chaitanya <56665907+ChaitanyaChandra@users.noreply.github.com>
2024-07-03 15:00:43 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,14 @@ sudo mv minikube /usr/local/bin/
 minikube version
 minikube start
 
+# crictl
 VERSION="v1.30.0"
 wget https://github.com/kubernetes-sigs/cri-tools/releases/download/$VERSION/crictl-$VERSION-linux-amd64.tar.gz
 sudo tar zxvf crictl-$VERSION-linux-amd64.tar.gz -C /usr/local/bin
 rm -f crictl-$VERSION-linux-amd64.tar.gz
+
+# zsh & ohmyzsh
+sudo apt-get install -y zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
