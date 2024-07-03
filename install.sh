23e9d7b400eeb5b31cc33380894b3b632a0d51e6
Chaitanya <56665907+ChaitanyaChandra@users.noreply.github.com>
2024-07-03 14:58:56 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,3 +6,9 @@ curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/miniku
 chmod +x minikube
 sudo mv minikube /usr/local/bin/
 minikube version
+minikube start
+
+VERSION="v1.30.0"
+wget https://github.com/kubernetes-sigs/cri-tools/releases/download/$VERSION/crictl-$VERSION-linux-amd64.tar.gz
+sudo tar zxvf crictl-$VERSION-linux-amd64.tar.gz -C /usr/local/bin
+rm -f crictl-$VERSION-linux-amd64.tar.gz
